db8a04ba36cfc2e8944b3bcd4be230589486f7ae
Katoa Apps <contact@katoaapps.com>
2026-08-22 19:31:17 -0700

    2026-08-22 MT. Release MinkLauncher OpenSource 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.katoaapps.openminilaunch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.2.0"
+        versionCode = 16
+        versionName = "1.2.1"
     }
 
     buildFeatures {
